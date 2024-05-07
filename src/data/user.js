77aa65baccc37dff7739e8f0807b48fe73d81663
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2024-05-07 16:10:51 -0400

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -163,7 +163,7 @@ const INFO = {
 			title: "Amazon",
 			subtitle: "Software Development Engineering Intern",
 			duration: "May 2024- present",
-			logo: "https://www.cdnlogo.com/logos/q/51/amazon.svg",
+			logo: "https://www.cdnlogo.com/logos/q/51/amazon-icon.svg",
 			long: "Analyzing customer insights to improve recommendations!"
 		},
 		{
