8ba17a70c7285268a69d3c9be97277b11c10049f
AbhiJ2706 <43829700+AbhiJ2706@users.noreply.github.com>
2024-05-09 09:44:01 -0400

    Update work.jsx

--- src/components/common/work.jsx
+++ src/components/common/work.jsx
@@ -6,13 +6,14 @@ import UWDSC from "../../img/uwdsc.png"
 import TOHacks from "../../img/tohacks.png"
 import WATai from "../../img/watai.jpg"
 import UW from "../../img/uw.png"
+import UWRL from "../../img/uwrl.png"
 
 const Work = (props) => {
     const { logo, title, subtitle, duration, long } = props;
 
     return (<div className={long == "" ? "work-small" : "work-big"}>
         <img
-            src={logo == "uwdsc" ? UWDSC : (logo == "tohacks" ? TOHacks : (logo == "watai" ? WATai : (logo == "uw" ? UW : logo)))}
+            src={logo == "uwdsc" ? UWDSC : (logo == "tohacks" ? TOHacks : (logo == "watai" ? WATai : (logo == "uw" ? UW : (logo == "uwrl" ? UWRL : logo))))}
             alt={title}
             className="work-image"
         />
